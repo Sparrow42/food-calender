c8480741f0f3bbfd8a1dda193040269e533f1065
toshihiro ban <btshr0326@gmail.com>
2021-04-18 00:50:16 +0900

    カレンダータイルへの日付対応画像の表示

--- src/components/pages/myCalendar.js
+++ src/components/pages/myCalendar.js
@@ -2,6 +2,20 @@ import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 
 export const MyCalendar = (props) => {
+  function createData(date, name, img) {
+    return { date, name, img };
+  }
+
+  const dateTemp1 = new Date(2021, 3, 12);
+  const dateTemp2 = new Date(2021, 3, 12);
+  const dateTemp3 = new Date(2021, 3, 16);
+
+  const rows = [
+    createData( dateTemp1, 'プリン', '/img/food_pudding.png'),
+    createData( dateTemp2, 'パン', '/img/food_bread.png'),
+    createData( dateTemp3, 'ラーメン', '/img/food_ramen.png')
+  ];
+
   //const [tileValue, setTileValue] = useState(<p>sample</p>);
   
   function onChange(nextValue) {
@@ -13,6 +27,19 @@ export const MyCalendar = (props) => {
   }
 
   const getTile = ({ activeStartDate, date, view }) => {
+    console.log(date.getDate());
+    const dispImg = rows.map((row) => {
+      if(view === 'month' &&
+      date.getFullYear() === row.date.getFullYear() && 
+      date.getMonth() === row.date.getMonth() && 
+      date.getDate() === row.date.getDate()){
+      
+        return (<div><img style={{width: 30}} src={row.img}></img></div>);
+      }
+    });
+
+    return dispImg;
+
     if(view === 'month' && date.getDay() === 0){
       return (<p>It's Sunday!</p>);
     }else{
